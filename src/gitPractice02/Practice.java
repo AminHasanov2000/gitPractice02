bc5581ebeaf841473866f86b53e8b8d6b24a1a04
AminHasanov2000 <147754079+AminHasanov2000@users.noreply.github.com>
2023-10-13 21:08:28 -0400

    Update Practice.java

--- src/gitPractice02/Practice.java
+++ src/gitPractice02/Practice.java
@@ -2,10 +2,6 @@ package gitPractice02;
 
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("asddsassd");
-        System.out.println("asddsassd");
-        System.out.println("asddsassd");
-        System.out.println("asddsassd");System.out.println("asddsassd");
-        System.out.println("asddsassd");
+       
     }
 }
